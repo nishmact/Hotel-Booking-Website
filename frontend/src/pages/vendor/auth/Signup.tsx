a2f43efa8a7ad111ae1335edd502ec6af92ed0c5
Nishma <nishmact3@gmail.com>
2025-01-10 13:28:59 +0530

    Updated Signup.tsx file

--- frontend/src/pages/vendor/auth/Signup.tsx
+++ frontend/src/pages/vendor/auth/Signup.tsx
@@ -203,33 +203,32 @@ const Signup = () => {
         <form onSubmit={submitHandler} className="space-y-4">
           {/* Logo Upload Field */}
           <div className="flex justify-center">
-            {logoPreview ? (
-              <img
-                src={logoPreview}
-                alt="Logo Preview"
-                className="h-25 w-25  object-cover mb-4"
-              />
-            ) : (
-              <label
-                htmlFor="logo-upload" // Make sure this matches the input's id
-                className="cursor-pointer h-40 w-40 flex items-center justify-center bg-gray-100 "
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={2}
-                  stroke="currentColor"
-                  className="h-12 w-12 text-gray-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                  />
-                </svg>
-              </label>
-            )}
+            <label htmlFor="logo-upload" className="cursor-pointer relative">
+              {logoPreview ? (
+                <img
+                  src={logoPreview}
+                  alt="Logo Preview"
+                  className="h-25 w-25 object-cover mb-4 rounded-full border border-gray-300"
+                />
+              ) : (
+                <div className="h-40 w-40 flex items-center justify-center bg-gray-100 rounded-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="h-12 w-12 text-gray-500"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                    />
+                  </svg>
+                </div>
+              )}
+            </label>
           </div>
 
           <input
